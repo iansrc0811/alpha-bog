ce2d4f9cb2c21c2e281eb9882cb9ab823cfc81c1
ian yeh <iansrc0811@hotmail.com>
2016-09-02 16:05:43 +0000

    Build Article and Category Association

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160830135812) do
+ActiveRecord::Schema.define(version: 20160902152347) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
