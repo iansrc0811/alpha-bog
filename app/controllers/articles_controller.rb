52100a61c110d2b3d3a35531c22b1d5185df97ab
ian yeh <iansrc0811@hotmail.com>
2016-09-04 13:52:28 +0000

    Add categories checkboxs in new Article page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -58,7 +58,8 @@ class ArticlesController < ApplicationController
     end
     
     def article_params
-      params.require(:article).permit(:title,:description)
+      params.require(:article).permit(:title,:description, category_ids: [])
+      #category_ids 以array傳送
     end
     
     def require_same_user
